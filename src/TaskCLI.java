d326860653f537977b9f25977c9985991f5e1802
Marina Jimenez <marinajimnz@outlook.com>
2025-08-07 14:32:17 +0200

    fix: added saveTasks method to cases that change json content + deleted unused imports

--- src/TaskCLI.java
+++ src/TaskCLI.java
@@ -1,12 +1,10 @@
-import java.util.ArrayList;
-
 public class TaskCLI {
     public static void main(String[] args) throws Exception {
         TaskManager taskManager = new TaskManager();
 
-        if(args.length < 1) {
-            throw new IllegalArgumentException("To use the task app you havo to write in the command line:\n" + 
-                                "java -cp bin TaskCLI <command> \\\"[arguments]");
+        if (args.length < 1) {
+            throw new IllegalArgumentException("To use the task app you havo to write in the command line:\n" +
+                    "java -cp bin TaskCLI <command> \\\"[arguments]");
         }
 
         String command = args[0].toLowerCase();
@@ -14,24 +12,29 @@ public class TaskCLI {
         switch (command) {
             case "add":
                 taskManager.addTask(args[1]);
+                taskManager.saveTasks();
                 break;
             case "update":
                 taskManager.updateTask(Integer.parseInt(args[1]), args[2]);
+                taskManager.saveTasks();
                 break;
             case "delete":
                 taskManager.deleteTask(Integer.parseInt(args[1]));
+                taskManager.saveTasks();
                 break;
             case "mark-in-progress":
                 taskManager.markInProgress(Integer.parseInt(args[1]));
+                taskManager.saveTasks();
                 break;
             case "mark-done":
                 taskManager.markDone(Integer.parseInt(args[1]));
+                taskManager.saveTasks();
                 break;
             case "list-done":
                 taskManager.listTasksByStatus(Status.DONE);
                 break;
             case "list-to-do":
-                taskManager.listTasksByStatus(Status.TO_DO);            
+                taskManager.listTasksByStatus(Status.TO_DO);
                 break;
             case "list-in-progress":
                 taskManager.listTasksByStatus(Status.IN_PROGRESS);
@@ -39,11 +42,11 @@ public class TaskCLI {
             case "list-all":
                 taskManager.listAllTasks();
                 break;
-                
+
             default:
                 System.out.println("Unknown command.");
                 break;
         }
-        
+
     }
 }
